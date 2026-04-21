2c179f80d230038367ac95409f2555daeed9b697
plbski <pl.buet@icloud.com>
2026-04-21 14:48:56 +0200

    correction syntaxe

--- infrastructure/databases/user-db/entrypoint.sql
+++ infrastructure/databases/user-db/entrypoint.sql
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 CREATE TYPE role_type AS ENUM (
     'admin',
@@ -22,9 +21,9 @@ CREATE TYPE conn_status AS ENUM (
 );
 
 CREATE TABLE users (
-	user_id			SERIAL			NOT NULL,
-	auth_id			INT				NOT NULL,
-	role			role_type,
+	user_id			SERIAL			NOT NULL PRIMARY KEY,
+	auth_id			INT				NOT NULL UNIQUE,
+	role			role_type		NOT NULL DEFAULT 'candidate',
 	firstname		VARCHAR(64)		NOT NULL,						-- 64 = convention standard
 	lastname		VARCHAR(64)		NOT NULL,
 	profile_pic_url	TEXT,
@@ -38,18 +37,16 @@ CREATE TABLE users (
 	email			VARCHAR(255)	NOT NULL UNIQUE,			--255 = RFC 5321
 	phone_number	VARCHAR(32),
 	is_active		BOOLEAN			NOT NULL DEFAULT TRUE,
-	last_login		TIMESTAMPTZ
+	last_login		TIMESTAMPTZ,
 	created_at		TIMESTAMPTZ		NOT NULL DEFAULT NOW(),
 	updated_at		TIMESTAMPTZ		NOT NULL DEFAULT NOW(),
 	deleted_at		TIMESTAMPTZ								--definie a null, sinon = utilisateur delete virtuellement interdir l'accès
-)
+);
 
 -- En POSTgreSql, pas de check possible pour verifier si candidat =candidat ou recruteur = recruteur, prevoir une fonction lors des insertion.
 CREATE TABLE connection(
-	recruiter_id	INT							NOT NULL,
-					REFERENCES users(user_id)
-	candidate_id	INT							NOT NULL,
-					REFERENCES user_id(user_id)
+	recruiter_id	INT							NOT NULL REFERENCES users(user_id),
+	candidate_id	INT							NOT NULL REFERENCES users(user_id),
 	status			conn_status					NOT NULL DEFAULT 'pending',
 	is_active		BOOLEAN						NOT NULL DEFAULT TRUE,
 	accepted_at		TIMESTAMPTZ,
@@ -59,4 +56,15 @@ CREATE TABLE connection(
 
 -- garantie (recruiter + candidate = unique)
 	PRIMARY KEY (recruiter_id, candidate_id)
-)
+);
+
+CREATE TABLE invite_links(
+	link_id			SERIAL			PRIMARY KEY,
+	recruiter_id	INT				NOT NULL REFERENCES users(user_id),
+	link			TEXT			NOT NULL UNIQUE,
+	expiry_date		TIMESTAMPTZ		NOT NULL,
+	created_at		TIMESTAMPTZ     NOT NULL DEFAULT NOW(),
+	updated_at		TIMESTAMPTZ     NOT NULL DEFAULT NOW()
+);
+
+--PK = reference pour la table -> une seul par table possible (equivault a NOT NULL UNIQUE)
