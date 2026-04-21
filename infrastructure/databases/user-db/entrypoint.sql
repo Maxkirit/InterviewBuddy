93b75de05d56781d0cd07fc6ce63b63ca0cf8cbb
plbski <pl.buet@icloud.com>
2026-04-21 14:32:09 +0200

    table connection

--- infrastructure/databases/user-db/entrypoint.sql
+++ infrastructure/databases/user-db/entrypoint.sql
@@ -22,24 +22,41 @@ CREATE TYPE conn_status AS ENUM (
 );
 
 CREATE TABLE users (
-	user_id			SERIAL			NOT NULL
-	auth_id			INT				NOT NULL
-	role			role_type
-	firstname		VARCHAR(64)								-- 64 = convention standard
-	lastname		VARCHAR(64)
-	profile_pic_url	TEXT
-	gender			gender_type
-	date_of_birth	DATE
-	country			VARCHAR
-	job_title		VARCHAR
-	organization	VARCHAR
-	bio				TEXT
-	linkedin_link	TEXT
-	email			VARCHAR(255)	NOT NULL UNIQUE			--255 = RFC 5321
-	phone_number	VARCHAR(32)
-	is_active		BOOLEAN
+	user_id			SERIAL			NOT NULL,
+	auth_id			INT				NOT NULL,
+	role			role_type,
+	firstname		VARCHAR(64)		NOT NULL,						-- 64 = convention standard
+	lastname		VARCHAR(64)		NOT NULL,
+	profile_pic_url	TEXT,
+	gender			gender_type,
+	date_of_birth	DATE,
+	country			VARCHAR(64),
+	job_title		VARCHAR(128),
+	organization	VARCHAR(128),
+	bio				TEXT,
+	linkedin_link	TEXT,
+	email			VARCHAR(255)	NOT NULL UNIQUE,			--255 = RFC 5321
+	phone_number	VARCHAR(32),
+	is_active		BOOLEAN			NOT NULL DEFAULT TRUE,
 	last_login		TIMESTAMPTZ
-	created_at		TIMESTAMPTZ		NOT NULL DEFAULT NOW()
-	updated_at		TIMESTAMPTZ		NOT NULL DEFAULT NOW()
+	created_at		TIMESTAMPTZ		NOT NULL DEFAULT NOW(),
+	updated_at		TIMESTAMPTZ		NOT NULL DEFAULT NOW(),
 	deleted_at		TIMESTAMPTZ								--definie a null, sinon = utilisateur delete virtuellement interdir l'accès
+)
+
+-- En POSTgreSql, pas de check possible pour verifier si candidat =candidat ou recruteur = recruteur, prevoir une fonction lors des insertion.
+CREATE TABLE connection(
+	recruiter_id	INT							NOT NULL,
+					REFERENCES users(user_id)
+	candidate_id	INT							NOT NULL,
+					REFERENCES user_id(user_id)
+	status			conn_status					NOT NULL DEFAULT 'pending',
+	is_active		BOOLEAN						NOT NULL DEFAULT TRUE,
+	accepted_at		TIMESTAMPTZ,
+	rejected_at		TIMESTAMPTZ,
+	created_at		TIMESTAMPTZ					NOT NULL DEFAULT NOW(),
+	updated_at		TIMESTAMPTZ					NOT NULL DEFAULT NOW(),
+
+-- garantie (recruiter + candidate = unique)
+	PRIMARY KEY (recruiter_id, candidate_id)
 )
