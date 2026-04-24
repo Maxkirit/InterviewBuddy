ee53203df630ba6c426db085bb8e3a08f44ab1a5
Maxkirit <maximeturgeon@ymail.com>
2026-04-24 11:57:29 +0200

    feat: push so I can have it at home

--- infrastructure/databases/interview-store-db/entrypoint.sh
+++ infrastructure/databases/interview-store-db/entrypoint.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-#copy paste your entry point script here. Can use .sql scripts if you prefer
+psql -U postgres --password=$(cat /run/secrets/interview_store_db_superuser_password.txt)
+
+# eventually add conditional checks for when DBs/TABLES exist already
+CREATE DATABASE [IF NOT EXISTS] $DB_NAME;
+\c $DB_NAME;
+CREATE TABLE [IF NOT EXISTS] $TABLE_INTERVIEWS (
+    unique_interview_id SERIAL PRIMARY KEY,
+    recruiter_id SERIAL FOREIGN KEY UNIQUE,
+    candidate_id SERIAL FOREIGN KEY NOT NULL,
+    question_id SERIAL FOREIGN KEY NOT NULL,
+    job_title TEXT NOT NULL,
+    unfinished_diagram JSON,
+    unfinished_text TEXT,
+    status ENUM ('scheduled', 'past due date', 'completed'),
+    due_date TIMESTAMP,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+)
