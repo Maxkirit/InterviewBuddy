dd19872544cd7b3f41e1d42192ceab4e96c7f616
plbski <pl.buet@icloud.com>
2026-04-29 14:34:11 +0200

    feat: get pubkey

--- services/svc-auth/dist/index.js
+++ services/svc-auth/dist/index.js
@@ -1,7 +1,21 @@
-import logPoint from "./practice_script.js";
-const point2 = {
-    x: 875,
-    y: 323
-};
-logPoint(point2);
-let arr = [];
+import express from 'express';
+import { exportJWK, importSPKI } from 'jose';
+import dotenv from 'dotenv';
+import { createPublicKey } from 'crypto';
+dotenv.config(); // {path: '...'} pour personnailiser ou est la cles
+const app = express();
+const port = 3000;
+const secret = process.env.SECRETKEY;
+if (!secret)
+    throw new Error('SECRETKEY manquante dans .env');
+app.get('/signing-key', async (req, res) => {
+    const publicKeyPem = createPublicKey(secret).export({ type: 'spki', format: 'pem' });
+    const publicKey = await importSPKI(publicKeyPem, 'RS256'); // ← extrait la clé publique en PEM
+    const jwk = await exportJWK(publicKey);
+    res.json({ keys: [{ ...jwk, use: 'sig', kid: 'svc-auth-key-1' }] });
+    // use: 'sig',                // ← indique que cette clé sert à signer (pas à chiffrer)
+    //    kid: 'svc-auth-key-1'    ← identifiant unique de la clé
+});
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
