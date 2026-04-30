b1ee32d638abb748aa2d3c27bc76d4b7f573e624
plbski <pl.buet@icloud.com>
2026-04-30 15:30:34 +0200

    feat : find auth id in userdb

--- services/svc-user/src/index.ts
+++ services/svc-user/src/index.ts
@@ -1,9 +1,27 @@
 import express from 'express'
+import { PrismaClient } from "./generated/users";
 
-const app= express;
+const prisma = new PrismaClient();
+const app= express();
 const port = 3000;
 
-app.get('api/v1/matchUserId', async (req, res) =>{
-	const {auth_id} = req.query;
-	
+
+app.get('/api/v1/matchUserId', async (req, res) =>{
+	const {auth_id} = req.query as {auth_id: string};
+	try{
+		const user = await prisma.user.findUnique({
+  		where: { auth_id: auth_id },
+		});
+		if(!user) res.status(401).json({error: "not find"})
+		res.json({user_id: user.user_id})
+	}
+	catch(e){
+		res.status(500).json({error: "internal error"})
+	}
+		
+
+})
+
+app.listen(port, ()=>{
+	console.log(`listening on port ${port}`)
 })
