fa98eb4ca111621fdbd7e3bf00c6c0bb1686db9a
plbski <pl.buet@icloud.com>
2026-05-03 16:09:10 +0200

    fix : import prima

--- services/svc-user/src/index.ts
+++ services/svc-user/src/index.ts
@@ -1,27 +1,22 @@
 import express from 'express';
-import { PrismaClient } from "./generated/prisma/index.js";
+import { prisma } from "./lib/prisma.js";
 
-const prisma = new PrismaClient();
-const app= express();
+const app = express();
 const port = 3000;
 
+app.get('/api/v1/matchUserId', async (req, res) => {
+  const { auth_id } = req.query as { auth_id: string };
+  try {
+    const user = await prisma.users.findUnique({
+      where: { auth_id: parseInt(auth_id, 10) },
+    });
+    if (!user) return res.status(401).json({ error: "not find" });
+    res.json({ user_id: user.user_id });
+  } catch (e) {
+    res.status(500).json({ error: "internal error" });
+  }
+});
 
-app.get('/api/v1/matchUserId', async (req, res) =>{
-	const {auth_id} = req.query as {auth_id: string};
-	try{
-		const user = await prisma.users.findUnique({
-  		where: { auth_id: parseInt(auth_id, 10) },
-		});
-		if(!user) return res.status(401).json({error: "not find"})
-		res.json({user_id: user.user_id})
-	}
-	catch(e){
-		res.status(500).json({error: "internal error"})
-	}
-		
-
-})
-
-app.listen(port, ()=>{
-	console.log(`listening on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
