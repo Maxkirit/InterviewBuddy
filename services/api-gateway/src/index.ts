38cbeb4d737babfb80b7a3c898b591890c9e586a
Maxkirit <maximeturgeon@ymail.com>
2026-05-04 18:41:11 +0200

    feat: regristration flow, commit while debugging

--- services/api-gateway/src/index.ts
+++ services/api-gateway/src/index.ts
@@ -10,6 +10,34 @@ type ApiError = {
   code: number;
 };
 
+<<<<<<< HEAD
+=======
+const passwordSchema = z.string()
+                        .min(8, {error: "Password too short", abort: true})
+                        .max(128, {error: "Password too long", abort: true})
+                        .refine((password) => /[A-Z]/.test(password), {error: "Missing at least 1 uppercase letter", abort: true})
+                        .refine((password) => /[a-z]/.test(password), {error: "Missing at least 1 lowercase letter", abort: true})
+                        .refine((password) => /[!@#$%^&*()+\-=[\]{};':"\\|,.<>/?~`]/.test(password), {error: "Missing at least 1 special character", abort: true});
+
+const nameSurnameSchema = z.string()
+                        .min(1, {error: "Name or surname field too short", abort: true})
+                        .max(64, {error: "Name or surname too long", abort: true});
+
+const Login = z.object({
+    email: z.email({error: "Wrong email format", abort: true}),
+    password: passwordSchema,
+});
+
+
+const NewUser = z.object({
+    email: z.email({error: "Wrong email format", abort: true}),
+    password: passwordSchema,
+    name: nameSurnameSchema,
+    surname: nameSurnameSchema,
+    role_type: z.string(),
+})
+
+>>>>>>> 72b6e07 (feat: regristration flow, commit while debugging)
 const app = express();
 const port = 3000;
 
@@ -23,13 +51,122 @@ app.use(express.json());
 // middleware for cookie parsing
 app.use(cookieParser());
 
+<<<<<<< HEAD
 app.use('/api/v1/auth', authRoute)
 
+=======
+app.get('/test', async(req, res) =>{
+    res.status(200).send("it works");
+})
+
+app.post('/api/v1/auth/login', async (req, res) => {
+    const result = Login.safeParse(req.body);
+    if (!result.success) {
+        return res.status(400).json({error: result.error});
+    }
+    try {
+        const response = await axios.post("http://svc-auth:3000/auth/auth-request", {
+            email: result.data.email,
+            password: result.data.password,
+        })
+        res.status(200);
+        res.cookie("refreshToken", response.data.refreshToken, {httpOnly: true, secure:  true, sameSite: "strict", maxAge: response.data.max_age}); //validity as cookie header, must be controlled by auth
+        return res.json({accessToken: response.data.accessToken, message: "Login succesful"});
+    } catch(error) {
+        if (axios.isAxiosError<ApiError>(error) && error.response?.status) {
+            return res.status(error.response.status).json({error: error.response.data.message});
+        } else {
+            return res.status(502).json({error: "Bad gateway"});
+        }
+    }
+})
+
+//registration flow
+app.post("/api/v1/auth/registration", async(req, res) => {
+    if (Object.keys(req.body).length == 0 || !req.body['email'] || !req.body['password'] || !req.body['name'] || !req.body['surname']){
+        return res.status(400).json({error: 'Missing signup info'});
+    }
+    const newUser = NewUser.safeParse(req.body); //name, surname, email and password validation
+    if (!newUser.success) {
+        return res.status(400).json({error: newUser.error});
+    }
+    try {
+        //auth will call svc-user to create user and return userid baked in access and refresh token
+        //auth then creates access token and returns it here
+        const response = await axios.post("http://svc-auth:3000/auth/user", {
+            email: req.body['email'],
+            password: req.body['password'],
+            name: req.body['name'],
+            surname: req.body['surname'],
+            role_type: req.body['role_type'], //'recruiter' or 'candidate' or 'admin'
+        });
+        res.cookie('refreshToken', response.data.refreshToken, {httpOnly: true, secure: true, sameSite: 'strict', maxAge: response.data.refreshMaxAge }); 
+        //return 201 to client and client queries updateProfile()
+        return res.status(201).json({accessToken: response.data.accessToken, message: "User registration succesful"});
+    } catch (error) {
+        if (axios.isAxiosError<ApiError>(error) && error.response?.status){
+            if (error.response.status === 409){
+                return res.status(error.response.status).json({error: 'User already registered'});
+            }
+            return res.status(error.response.status).json({error: error.response.data.message});
+        }
+        return res.status(502).json({error: "Bad gateway"});
+    }
+})
+
+>>>>>>> 72b6e07 (feat: regristration flow, commit while debugging)
 // signup before validateAcccessToken
 app.use(validateAcccessToken);
 // other routes after (needs verification) 
 
+<<<<<<< HEAD
 app.use('/api/v1/auth', protectedAuthRoute)
+=======
+app.get("/api/v1/auth/refresh", async(req, res) => {
+    console.log(req.cookies);
+    if (Object.keys(req.cookies).length === 0 || !req.cookies['refreshToken']){ //used to be JSON.stringify(req.cookies) === '{}' but checking if object has keys is cleaner
+        console.log("no refresh cookie header\n"); //res.send() closes the HTTP connection but without return code keeps gind --> error
+        return res.status(404).json({error: "refresh token not found"});
+    }
+    const oldRefreshToken = req.cookies['refreshToken']; //store so we can send back with max_age==-1 to invalidate old ones in browser
+    try {
+        const result = await axios.post("http://svc-auth:3000/auth/refresh-token", {
+            refreshToken: oldRefreshToken,
+        });
+        res.json({accessToken: result.data.accessToken, message: 'Refresh successful'});
+        return res.cookie("refreshToken", result.data.refreshToken, {httpOnly: true, secure:  true, sameSite: "strict", maxAge: result.data.refreshMaxAge});
+    } catch (error){
+        if (axios.isAxiosError<ApiError>(error) && error.response?.status){
+            return res.status(error.response.status).json({error: error.response.data.message});
+        } else {
+            return res.status(502).json({error: "Bad gateway"});
+        }
+    }
+})
+
+//close SSE connections, propagate logout state after refresh token validation
+app.get("/api/v1/auth/logout", async(req, res) => {
+    // use expired refresh token for identity. That's why we should only send a maxAge == -1 AFTER logout or with another refresh token.
+    if (Object.keys(req.cookies).length === 0 || !req.cookies['refreshToken']){ 
+        return res.status(404).json({error: "refresh token not found"});
+    }
+    const refreshToken = req.cookies['refreshToken'];
+    try{
+        const response = await axios.patch("http://svc-auth:3000/auth/refresh-token", { //returns userId
+            refreshToken: refreshToken,
+        });
+        //post message to RabbitMQ
+        res.cookie("refreshToken", refreshToken, {httpOnly: true, secure: true, sameSite: "strict", maxAge: -1});
+        return res.status(200).json({message: 'Logout successful'});
+    } catch (error) {
+        if (axios.isAxiosError<ApiError>(error) && error.response?.status){
+            return res.status(error.response.status).json({error: error.response.data.message});
+        } else {
+            return res.status(502).json({error: "Bad gateway"});
+        } 
+    }
+})
+>>>>>>> 72b6e07 (feat: regristration flow, commit while debugging)
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
