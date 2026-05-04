60dffd5ae946aa941991352ab6c9ee9e3c4cdae1
plbski <pl.buet@icloud.com>
2026-05-04 08:51:16 +0200

    fix: clean code + norme

--- services/svc-user/src/index.ts
+++ services/svc-user/src/index.ts
@@ -5,18 +5,18 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/matchUserId', async (req, res) => {
-  const { auth_id } = req.query as { auth_id: string };
-  try {
-    const user = await prisma.users.findUnique({
-      where: { auth_id: parseInt(auth_id, 10) },
-    });
-    if (!user) return res.status(401).json({ error: "not find" });
-    res.json({ user_id: user.user_id });
-  } catch (e) {
-    res.status(500).json({ error: "internal error" });
-  }
+	const { auth_id } = req.query as { auth_id: string };
+	try {
+		const user = await prisma.users.findUnique({
+			where: { auth_id: parseInt(auth_id, 10) },
+		});
+		if (!user) return res.status(401).json({ error: "not find" });
+		res.json({ user_id: user.user_id });
+	} catch (e) {
+		res.status(500).json({ error: "internal error" });
+	}
 });
 
 app.listen(port, () => {
-  console.log(`listening on port ${port}`);
+	console.log(`listening on port ${port}`);
 });
