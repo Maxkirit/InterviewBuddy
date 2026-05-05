413f85ebc086cd429738db362dfe29860d1a8869
Maxkirit <maximeturgeon@ymail.com>
2026-05-05 10:45:43 +0200

    feat: remove entry in auth db on error path

--- services/svc-user/src/index.ts
+++ services/svc-user/src/index.ts
@@ -2,9 +2,11 @@
 import express from 'express';
 import { prisma, Prisma } from "./lib/prisma.js";
 import { role_type } from './generated/prisma/enums.js';
+import { json } from 'node:stream/consumers';
 
 const app = express();
 const port = 3000;
+ app.use(express.json());
 
 app.get('/api/v1/matchUserId', async (req, res) => {
 	const { auth_id } = req.query as { auth_id: string };
@@ -19,18 +21,22 @@ app.get('/api/v1/matchUserId', async (req, res) => {
 	}
 });
 
-app.post('/svc-user/profile/:auth_id', async (req, res) =>{
+app.post('/svc-user/profile/:auth_id', async (req, res) => {
     console.log("in post new user\n");
     //validate permissions ? technically no access tokens in the request
     try {
         console.log("create new user in db\n");
         const authId = parseInt(req.params.auth_id as string);
+        console.log("after const authid\n");
         if (isNaN(authId)) {
+            console.log("isNaN\n");
             return res.status(400).json({ error: "Invalid auth_id" });
         }
-        const role = req.body.role_type;
+        console.log(`authId valid, checking role: ${req.body.role_type}\n`);
+        const role: role_type = req.body.role_type;
         if (role !== 'recruiter' && role !== 'candidate' && role !== 'admin')
-                return res.status(400).json({error: "Invalid role type"});
+            return res.status(400).json({error: "Invalid role type"});
+        console.log("role_type validated\n");
         const newUser = await prisma.users.create({
             data: {
                 auth_id: authId,
@@ -44,7 +50,6 @@ app.post('/svc-user/profile/:auth_id', async (req, res) =>{
         return res.status(201).json({userId: newUser.user_id, message: "User created"});
     } catch (error) { //gigachad catch path made by papa claude cuz i was tired as hell
     console.error("new user error path:", error);
-
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
         // e.g. P2002 = unique constraint (duplicate email/auth_id)
         if (error.code === 'P2002') {
@@ -65,7 +70,7 @@ app.post('/svc-user/profile/:auth_id', async (req, res) =>{
 
     // Catch-all: always have this as your last line
     return res.status(500).json({ error: "Internal server error" });
-}
+    }
 })
 
 app.listen(port, () => {
