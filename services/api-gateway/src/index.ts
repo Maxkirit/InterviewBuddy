c18993dbee652c1db0673f3a4a7f18862aa39453
Maxkirit <maximeturgeon@ymail.com>
2026-05-18 13:51:22 +0200

    feat: base monitoring for all services

--- services/api-gateway/src/index.ts
+++ services/api-gateway/src/index.ts
@@ -7,6 +7,7 @@ import userRoute from './router/user.route.js'
 import protectedAuthRoute from './router/protected-auth.route.js';
 import interviewRoute from './router/interview.route.js';
 import gradingRoute from './router/grading.route.js';
+import { monitoringMiddleware, monitor } from './metrics.js';
 
 export type ApiError = {
   error: string,
@@ -25,11 +26,14 @@ app.use(express.json());
 // middleware for cookie parsing
 app.use(cookieParser());
 
+//monitoring 
+app.use(monitoringMiddleware);
+app.use('/api/v1/use', monitor);
+
 app.use('/api/v1/auth',authRoute);
 // // signup before validateAcccessToken
 app.use(validateAcccessToken);
 // // other routes after (needs verification) 
-
 app.use('/api/v1/auth', protectedAuthRoute);
 app.use('/api/v1/user', userRoute);
 app.use('/api/v1/interview', interviewRoute);
